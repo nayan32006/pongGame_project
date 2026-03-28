26d435a9341d1e00569d2781498eb489c091de80
nayan32006 <nayang389@gmail.com>
2026-03-28 19:53:25 +0530

    day 2: paddle movement and ball working

--- script.js
+++ script.js
@@ -4,31 +4,73 @@ const ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height= 400;
 
+// ball
 let x=50;
 let y=50;
 let radius = 10;
+ if(y + radius > canvas.height || y-radius < 0){
+        dy = -dy;
+    }
 
-ctx.beginPath();
-ctx.arc(x, y, radius, 0, Math.PI * 2);
+//paddle
+let paddleWidth = 10;
+let paddleHeight =80;
+let paddleY =150;
 
-ctx.fillStyle = "red";
-ctx.fill();
-
-
-function drawBall() {
-   
-
-    ctx.biginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    
-    ctx.fillStyle = "red";
-    ctx.fill();
-    ctx.stroke();
+//mouse control
+document.addEventListener("mousemove", function(e) {
+    console.log("mouse moving")
+    paddleY = e.clientY - paddleHeight /2 ;
 
 
-   
+// limit paddle inside canvas
+if(paddleY < 0) paddleY=0;
+if(paddleY > canvas.height - paddleHeight){
+    paddleY = canvas.height - paddleHeight;
 }
 
-drawBall();
+});
+
+//draw function
+
+
+function draw() {
+    ctx.clearRect(0,0 ,canvas.width ,canvas.height);
+   
+    // ball
+    ctx.beginPath();
+    ctx.arc(x, y, radius,10, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    //paddle
+    ctx.fillRect(10, paddleY, 10, 80);
+
+    //move ball
+    x += 2;
+    y += 2;
+
+    //wall collision
+    if(y + radius > canvas.height || y-radius < 0){
+        dy = -dy;
+    }
+
+    //paddle collision
+    if (x -radius < 20 && y> paddleY && y < paddleY + paddleHeight) {
+        dx = -dx;
+    }
+
+    // right wall bounce
+    if(x + radius > canvas.width) {
+        dx = -dx ;
+    }
+
+    requestAnimationFrame(draw);
+}
+
+    
+
+// start game
+draw();
 
 
